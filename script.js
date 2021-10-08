20a2238beeb5060b0c57b5cd5d1644367aaf2dcf
J <jlin9533@gmail.com>
2021-10-07 20:10:30 -0400

    linked newsletter to index

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// take away the message if user input text or focus
 function clearmsg() {
     var msg = document.getElementById("email");
     if(msg.value == "Enter your email") {
@@ -5,9 +6,29 @@ function clearmsg() {
     }
 }
 
+// show a "enter your email" message if the input field is empty and blur
 function msgback() {
     var msg = document.getElementById("email");
     if(msg.value == "") {
         msg.value = "Enter your email";
     }
 }
+
+// get nav bar by ajax
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "nav.js",
+        dataType: "json",
+        success: function(responseData, status) {
+            var output="";
+            output += responseData.landing + "<ul><li>";
+            output += responseData.gnh + "</li><li>";
+            output += responseData.events + "</li><li>";
+            output += responseData.newsletter + "</li><li>";
+            output += responseData.tutoring + "</li><ul>";
+            $("#nav_bar").html(output);
+        }
+    });
+
+})
